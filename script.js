a2239b13740f38552c85d511bdb9714145a1542e
Justin Yip <yipju@bc.edu>
2021-09-23 22:23:32 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// Load data
+d3.csv('cities.csv').then(data=>{
+    console.log('cities',data);
+  })
+
+/*d3.csv('cities.csv', d=>{
+  return {
+    ...d, // spread operator
+    eu: d.eu==='true', // convert to boolean
+    population: +d.population,
+    x: +d.x,
+    y: +d.y,
+  }
+}).then(data=>{
+	console.log('cities', data);
+})
+*/
